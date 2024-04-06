d98ac61b70a0ffe02351eb14cb5483364c5f60e2
Gagan <91519308+GaganLohchab@users.noreply.github.com>
2024-04-07 00:58:51 +0530

    Frontend pages added

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  // eslint-disable-next-line no-undef
+  plugins: [require("daisyui")],
+}
