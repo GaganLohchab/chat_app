e0abe6d2b15fed9bcc8e1489547a3c302198dd33
Gagan <91519308+GaganLohchab@users.noreply.github.com>
2024-04-08 00:24:02 +0530

    Fronend all pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,17 @@
-// import "./App.css";
+import "./App.css";
+import Home from "./pages/home/Home";
 // import "./index.css";
+
+// import Login from "./pages/Login/Login";
+// import SignUp from "./pages/signup/SignUp";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-      <p className="font-bold text-6xl">hello Girls</p>
+      <Home />
     </div>
   );
 }
 
 export default App;
+
