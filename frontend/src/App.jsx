beb8d171e8aacb923c007852f8234e0c7e9fdaae
Gagan <91519308+GaganLohchab@users.noreply.github.com>
2024-04-09 20:00:26 +0530

    connecting frontend and backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
 import { Routes, Route } from "react-router-dom";
+import { Navigate } from 'react-router-dom';
 import "./App.css";
-// import "./index.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/signup/SignUp";
 import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
 
 function App() {
+  const { authUser } = useAuthContext();
+
   return (
+    
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={authUser ? <Home /> : <Navigate to={"/login"}/>} />
+        <Route path="/login" element={authUser ? <Navigate to="/"/> :<Login />} />
+        <Route path="/signup" element={authUser ? <Navigate to="/"/> : <SignUp />} />
       </Routes>
       <Toaster/>
 
--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
-// import { useAuthContext } from "../context/AuthContext";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
 	const [loading, setLoading] = useState(false);
-	// const { setAuthUser } = useAuthContext();
+	const { setAuthUser } = useAuthContext();
 
 	const signup = async ({ fullName, username, password, confirmPassword, gender }) => {
 		const success = handleInputErrors({ fullName, username, password, confirmPassword, gender });
@@ -19,12 +19,11 @@ const useSignup = () => {
 			});
 
 			const data = await res.json();
-            console.log(data);
-			// if (data.error) {
-			// 	throw new Error(data.error);
-			// }
-			// localStorage.setItem("chat-user", JSON.stringify(data));
-			// setAuthUser(data);
+			if (data.error) {
+				throw new Error(data.error);
+			}
+			localStorage.setItem("chat-user", JSON.stringify(data));
+			setAuthUser(data);
 		} catch (error) {
 			toast.error(error.message);
 		} finally {
