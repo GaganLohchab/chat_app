f822ca1cab7d8b016725f1fde88e3ef6ae5f75fd
Gagan <91519308+GaganLohchab@users.noreply.github.com>
2024-04-11 22:32:44 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -14,6 +15,8 @@ import {app, server} from "./socket/socket.js"
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // Middleware for parsing JSON
@@ -22,6 +25,12 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", usersRoutes)
+
+app.use(express.static(path.join(__dirname, "./frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend","dist", "index.html"));
+});
 
 // app.get("/", (req, res) => {
 //      // root route http://localhost:5000
