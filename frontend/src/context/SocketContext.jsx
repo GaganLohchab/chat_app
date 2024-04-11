af1363884110a245d8b81218c32ecb173149715b
Gagan <91519308+GaganLohchab@users.noreply.github.com>
2024-04-12 00:27:29 +0530

    another error

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -18,7 +18,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("https://chat-app-steo.onrender.com/",{
+            const socket = io("http://localhost:5000",{
                 query: {
                     userId: authUser._id,
                 },
